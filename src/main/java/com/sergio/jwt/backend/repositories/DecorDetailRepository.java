f823c77ad7ad62fa8a22e28838ac8bce6d031061
Samarth Solanki <131291766+SamarthSolanki1@users.noreply.github.com>
2025-01-20 22:22:42 +0530

    Delete src directory

--- src/main/java/com/sergio/jwt/backend/repositories/DecorDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sergio.jwt.backend.repositories;
-
-import com.sergio.jwt.backend.entites.DecorDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DecorDetailRepository extends JpaRepository<DecorDetail, Long> {
-}
