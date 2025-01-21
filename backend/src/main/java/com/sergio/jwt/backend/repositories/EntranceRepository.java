eac13039667c4a6d03ab90bf064920a4aed298b2
Samarth01 <samarthsolanki56@gmail.com>
2025-01-21 19:50:01 +0530

    samarth side on 21/2/25

--- backend/src/main/java/com/sergio/jwt/backend/repositories/EntranceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sergio.jwt.backend.repositories;
-
-import com.sergio.jwt.backend.entities.EntranceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EntranceRepository extends JpaRepository<EntranceEntity, Long> {
-}
--- backend/src/main/java/com/sergio/jwt/backend/repositories/MandapRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sergio.jwt.backend.repositories;
-
-import com.sergio.jwt.backend.entites.MandapEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MandapRepository extends JpaRepository<MandapEntity, Long> {
-}
