0bc58db83f75821f847d237d8a9f8c4b476a0f24
aryansoni2811 <145922291+aryansoni2811@users.noreply.github.com>
2025-01-24 18:34:04 +0530

    Repository for employee side

--- backend_S/src/main/java/com/sergio/jwt/backend/repositories/DiningRepository.java
+++ backend_S/src/main/java/com/sergio/jwt/backend/repositories/DiningRepository.java
@@ -1,9 +1,9 @@
 package com.sergio.jwt.backend.repositories;
 
-import com.sergio.jwt.backend.entites.VenueEntity;
+import com.sergio.jwt.backend.entites.DiningEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VenueRepository extends JpaRepository<VenueEntity, Long> {
+public interface DiningRepository extends JpaRepository<DiningEntity, Long> {
 }
--- backend_S/src/main/java/com/sergio/jwt/backend/repositories/LightingRepository.java
+++ backend_S/src/main/java/com/sergio/jwt/backend/repositories/LightingRepository.java
@@ -1,9 +1,9 @@
 package com.sergio.jwt.backend.repositories;
 
-import com.sergio.jwt.backend.entites.VenueEntity;
+import com.sergio.jwt.backend.entities.LightingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VenueRepository extends JpaRepository<VenueEntity, Long> {
+public interface LightingRepository extends JpaRepository<LightingEntity, Long> {
 }
