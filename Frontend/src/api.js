7a7697601f47b56d3ed7586684dfee557c02ab37
Samarth01 <samarthsolanki56@gmail.com>
2025-04-29 13:14:38 +0530

    changed api url for backend

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // Replace with your backend base URL
+  baseURL: "https://dream-knot.onrender.com", // Replace with your backend base URL
   headers: {
     "Content-Type": "application/json",
   },
